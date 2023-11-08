047a2955f626d7d826f2ba60c94475f628756916
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-11-08 12:05:01 -0300

    update module: model add mustipart forma data send

---
+++ send.go
@@ -0,0 +1,7 @@
+package httpclient
+
+import "github.com/cdvelop/model"
+
+func (h HttpClient) SendFormData(object_name string, data map[string]interface{}, out_resp func(model.Response, error)) {
+
+}
